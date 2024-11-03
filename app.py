5abb437a68f4660a5fd8e9e34f7df3582dbda2eb
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2024-11-03 21:03:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,4 @@ vertexai.init(project='prj-mygcpproject-219-8a4e', location='europe-west4')
 from vertexai.generative_models import GenerativeModel
 model = GenerativeModel("gemini-pro")
 st.write(model.generate_content("Why is sky blue?"))
+st.write("Hello again")
