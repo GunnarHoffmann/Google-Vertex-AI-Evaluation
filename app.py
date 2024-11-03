753e6cff1e0e38b898d85e17090bc1545d58c41e
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2024-11-03 21:22:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,9 @@ location = st.secrets["google_cloud"]["location"]
 aiplatform.init(project=project_id, location=location, credentials=vertex_ai_credentials)
 
 vertexai.init(project='prj-mygcpproject-219-8a4e', location='europe-west4')
-
-
-
 from vertexai.generative_models import GenerativeModel
+
+
 model = GenerativeModel("gemini-1.5-flash-001")
 st.write("Hi")
 st.write(model.generate_content("Why is sky blue?").text)
