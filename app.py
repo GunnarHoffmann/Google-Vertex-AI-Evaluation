a56fd8cd4c49bf37cdc53bd5ca7fde7a7106e99f
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2024-11-03 21:11:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ import streamlit
 import vertexai
 import streamlit as st
 
+# Configure Google Vertex AI for Gemini
+vertex_ai_credentials_json = st.secrets["vertex_ai_key"]
+vertex_ai_credentials_info = json.loads(vertex_ai_credentials_json)
+vertex_ai_credentials = service_account.Credentials.from_service_account_info(vertex_ai_credentials_info)
+project_id = st.secrets["google_cloud"]["project_id"]
+location = st.secrets["google_cloud"]["location"]
+aiplatform.init(project=project_id, location=location, credentials=vertex_ai_credentials)
+
 vertexai.init(project='prj-mygcpproject-219-8a4e', location='europe-west4')
+
+
 
 from vertexai.generative_models import GenerativeModel
 model = GenerativeModel("gemini-1.5-flash-001")
