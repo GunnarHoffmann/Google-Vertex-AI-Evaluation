a2473fd268ea1aea0c6cdf39ed3b81592b718d22
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2024-11-03 20:57:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit
 import vertexai
 
-vertexai.init(project='my-project', location='us-central1')
+vertexai.init(project='my-project', location='europe-west4')
 
 from vertexai.generative_models import GenerativeModel
 model = GenerativeModel("gemini-pro")
