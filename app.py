5a06325c4e19cc7bc382dff67e213b1ab2823dea
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2024-11-03 21:05:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 vertexai.init(project='prj-mygcpproject-219-8a4e', location='europe-west4')
 
 from vertexai.generative_models import GenerativeModel
-model = GenerativeModel("gemini-pro")
+model = GenerativeModel("gemini-1.5-flash-001")
 st.write("Hi")
 st.write(model.generate_content("Why is sky blue?"))
 st.write("Hello again")
