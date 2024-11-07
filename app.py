37be6dbec20d6299ecfd2939ba42a8f72363b95b
GunnarHoffmann <47139778+GunnarHoffmann@users.noreply.github.com>
2024-11-07 11:29:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ import json
 from google.oauth2 import service_account
 from google.cloud import aiplatform
 
-# Headline
-st.image("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png", width=100)
-st.image("https://companieslogo.com/img/orig/EONGY_BIG-6bfae4b4.png?t=1632523690", width=100)
-st.title("Welcome Gert")
-
 # Configure Google Vertex AI for Gemini
 vertex_ai_credentials_json = st.secrets["vertex_ai_key"]
 vertex_ai_credentials_info = json.loads(vertex_ai_credentials_json)
@@ -21,10 +16,7 @@ aiplatform.init(project=project_id, location=location, credentials=vertex_ai_cre
 vertexai.init(project='prj-mygcpproject-219-8a4e', location='europe-west4')
 from vertexai.generative_models import GenerativeModel
 
-model = GenerativeModel("gemini-1.5-flash-001")
 
-# Create an input field for user question
-user_question = st.text_input("Enter your question:")
-if user_question:
-    response = model.generate_content(user_question).text
-    st.write(f"Answer by Google Gemini Flash: {response}")
+model = GenerativeModel("gemini-1.5-flash-001")
+st.write("Why is the sky blue?")
+st.write(f"Answer by Google Gemini Flash: {model.generate_content("Why is sky blue?").text}")
